940e76f48efc2a58eabd2f43871ececa3ee62e85
amcelroy <mcelroy.austin@gmail.com>
2017-12-31 21:14:14 -0600

    Helpful support files

---
+++ Pi Support/s.sh
@@ -0,0 +1,1 @@
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=10000,server=y,suspend=n -jar /tahi/payara-micro.jar --deploy /tahi/LeptonWebApp-1.0-SNAPSHOT.war --logToFile /tahi/payara.log --disablephonehome --nocluster
